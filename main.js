32905390e6f61fa02f3bb883b9b156494bc40b2a
Vanessa <machado.vanessa32@gmail.com>
2022-02-26 11:32:48 -0300

    aulas

---
+++ main.js
@@ -0,0 +1,11 @@
+//se a constant esta com um nome, logo eu nao posso tentar modificar.
+
+const pessoa = {
+	name: 'mayk',
+	age: 37,
+	falar: function () {
+		console.log(pessoa.age)
+	}
+}
+
+pessoa.falar()
